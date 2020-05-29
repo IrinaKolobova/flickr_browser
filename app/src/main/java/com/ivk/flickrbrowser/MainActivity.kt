c0a0e9b4066120fa2fc61eb0ef1fe4472da660fc
Irina Kolobova <kolobovairinav@gmail.com>
2020-05-28 20:42:29 -0700

    Add Uri Builder

--- app/src/main/java/com/ivk/flickrbrowser/MainActivity.kt
+++ app/src/main/java/com/ivk/flickrbrowser/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.ivk.flickrbrowser
 
+import android.location.Criteria
+import android.net.Uri
+import android.nfc.NdefRecord.createUri
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -19,10 +22,24 @@ class MainActivity : AppCompatActivity(), GetRawData.OnDownloadComplete, GetFlic
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val url = createUri("https://api.flickr.com/services/feeds/photos_public.gne", "android,oreo", "en-us", true)
         val getRawData = GetRawData(this)
-        getRawData.execute("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,oreo&format=json&nojsoncallback=1")
+        getRawData.execute(url)
 
         Log.d(TAG, "onCreate ends")
+    }
+
+    private fun createUri(baseURL: String, searchCriteria: String, lang: String, matchAll: Boolean): String {
+        Log.d(TAG, ".createUri starts")
+
+        return Uri.parse(baseURL)
+                .buildUpon().
+                appendQueryParameter("tags", searchCriteria).
+                appendQueryParameter("tagmode", if(matchAll) "ALL" else "ANY").
+                appendQueryParameter("lang", lang).
+                appendQueryParameter("format", "json").
+                appendQueryParameter("nojsoncallback", "1").
+                build().toString()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
